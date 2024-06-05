0bba65dc160fec07e657b9d3a56b01f28daa990e
SagidKamilov <89615854074a@gmail.com>
2024-06-05 16:54:27 +0300

    Добавил параметры первичных ключей для полей таблицы посредника

--- src/model/user_category_list.py
+++ src/model/user_category_list.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.model.base_model import Base
 
@@ -7,5 +7,5 @@ from src.model.base_model import Base
 class UserCategoryList(Base):
     __tablename__ = "user_category_list_word_card"
 
-    user_id: Mapped[int] = mapped_column("user_id", ForeignKey("user_word_card.id"))
-    category_id: Mapped[int] = mapped_column("category_id", ForeignKey("category_word_card.id"))
+    user_id: Mapped[int] = mapped_column("user_id", ForeignKey("user_word_card.id"), primary_key=True)
+    category_id: Mapped[int] = mapped_column("category_id", ForeignKey("category_word_card.id"), primary_key=True)
