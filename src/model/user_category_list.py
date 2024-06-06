985db28ceec81d6a2ce56006299c70742d6486a4
SagidKamilov <89615854074a@gmail.com>
2024-06-06 11:24:51 +0300

    Добавил каскадное удаление данных, при удалении категории или пользователя

--- src/model/user_category_list.py
+++ src/model/user_category_list.py
@@ -7,5 +7,5 @@ from src.model.base_model import Base
 class UserCategoryList(Base):
     __tablename__ = "user_category_list_word_card"
 
-    user_id: Mapped[int] = mapped_column("user_id", ForeignKey("user_word_card.id"), primary_key=True)
-    category_id: Mapped[int] = mapped_column("category_id", ForeignKey("category_word_card.id"), primary_key=True)
+    user_id: Mapped[int] = mapped_column("user_id", ForeignKey("user_word_card.id", ondelete="CASCADE"), primary_key=True)
+    category_id: Mapped[int] = mapped_column("category_id", ForeignKey("category_word_card.id", ondelete="CASCADE"), primary_key=True)
