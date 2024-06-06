e2577a7501ec1b6dacca18eae6d107779ac0a0ce
SagidKamilov <89615854074a@gmail.com>
2024-06-07 00:39:31 +0300

    файлы инициализации

---
+++ src/api/endpoints/__init__.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from src.api.endpoints.user import router as user_router
+from src.api.endpoints.category import router as category_router
+from src.api.endpoints.word import router as word_router
+
+
+main_router = APIRouter()
+
+main_router.include_router(user_router)
+main_router.include_router(category_router)
+main_router.include_router(word_router)
