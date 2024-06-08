f726a8adf55e4e13c82907af719c0f9625b57f41
SagidKamilov <89615854074a@gmail.com>
2024-06-08 16:03:41 +0300

    Создал контейнеры для категории и пользователя

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -4,6 +4,12 @@ from src.service.user import UserService
 from src.repository.user import UserRepository
 from src.security.hash_password import HashGenerator
 
+from src.service.category import CategoryService
+from src.repository.category import CategoryRepository
+
+from src.service.word import WordService
+from src.repository.word import WordRepository
+
 
 async_database = AsyncDatabase()
 async_database.initialize_engine()
@@ -18,3 +24,18 @@ def user_container() -> UserService:
 
     return user_service
 
+
+def category_container() -> CategoryService:
+    async_session = async_database.get_session()
+    category_repository = CategoryRepository(async_db_session_obj=async_session())
+    category_service = CategoryService(category_repo=category_repository)
+
+    return category_service
+
+
+def word_container() -> WordService:
+    async_session = async_database.get_session()
+    word_repository = WordRepository(async_db_session_obj=async_session())
+    word_service = WordService(word_repo=word_repository)
+
+    return word_service
