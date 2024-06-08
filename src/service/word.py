6f0b5b1df5ac2c9c9ba239d0fcccbc5f134945f8
SagidKamilov <89615854074a@gmail.com>
2024-06-08 16:04:41 +0300

    Изменил методы сервиса

--- src/service/word.py
+++ src/service/word.py
@@ -9,116 +9,74 @@ class WordService:
     def __init__(self, word_repo: WordRepository):
         self.word_repo = word_repo
 
-    async def create_user(self, word_service: WordCreate) -> WordResponse:
+    async def create_word(self, word_create: WordCreate) -> WordResponse:
+        word = await self.word_repo.create_word(word_create=word_create)
 
-
-
-    async def verify_user(self, user_auth: UserAuth) -> UserResponse:
-        user: User = await self.word_repo.get_user_by_email(user_email=user_auth.email)
-
-        if not user:
-            raise Exception(f"Пользователя с email `{user_auth.email}` не существует!")
-
-        result = await self.verify_password(password=user_auth.password, hash_password=user.hash_password)
-
-        if not result:
-            raise Exception("Неверный пароль! Повторите попытку")
-
-        return UserResponse(
-            id=user.id,
-            username=user.username,
-            email=user.email,
-            birthday=user.birthday
+        return WordResponse(
+            id=word.id,
+            main_language=word.main_language,
+            second_language=word.second_language,
+            transcription=word.transcription
         )
 
-    async def get_user(self, user_id: int) -> UserResponse:
-        user: User = await self.word_repo.get_user_by_id(user_id=user_id)
+    async def get_word(self, word_id: int) -> WordResponse:
+        word = await self.word_repo.get_word_by_id(word_id=word_id)
 
-        if not user:
-            raise Exception(f"Пользователь с id = `{user_id}` не был найден!")
+        if not word:
+            raise Exception(f"Слово с id = `{word_id}` не найдено!")
 
-        return UserResponse(
-            id=user.id,
-            username=user.username,
-            email=user.email,
-            birthday=user.birthday
+        return WordResponse(
+            id=word.id,
+            main_language=word.main_language,
+            second_language=word.second_language,
+            transcription=word.transcription
         )
 
-    async def get_users(self) -> List[UserResponse]:
-        users = await self.word_repo.get_users()
+    async def get_words(self, category_id: int) -> List[WordResponse]:
+        words = await self.word_repo.get_words_by_category_id(category_id=category_id)
 
-        users_response_list = [
-            UserResponse(
-                id=user.id,
-                username=user.username,
-                email=user.email,
-                birthday=user.birthday
+        response_words = [
+            WordResponse(
+                id=element.id,
+                main_language=element.main_language,
+                second_language=element.second_language,
+                transcription=element.transcription
             )
-            for user
-            in users
+            for element
+            in words
         ]
 
-        return users_response_list
+        return response_words
 
-    async def update_user(self, user_id: int, user_update: UserUpdate) -> UserResponse:
-        check_user_exist: bool = await self.check_user_exist(user_id=user_id)
+    async def update_word(self, word_id: int, word_update: WordUpdate) -> WordResponse:
+        check_exist = await self.check_word_exist(word_id=word_id)
 
-        if not check_user_exist:
-            raise Exception(f"Пользователь с id = `{user_id}` не был найден!")
+        if not check_exist:
+            raise Exception(f"Слово с id = `{word_id}` не было найдено!")
 
-        check_duplicate: bool = await self.check_duplicate(user_email=user_update.email)
+        word = await self.word_repo.update_word(word_id=word_id, word_update=word_update)
 
-        if check_duplicate:
-            raise Exception(f"Email `{user_update.email}` занят!")
-
-        hashed_password: str = await self.hash_password(password=user_update.password)
-
-        user_update_hash_password = UserUpdate(
-            username=user_update.username,
-            password=hashed_password,
-            email=user_update.email,
-            birthday=user_update.birthday
+        return WordResponse(
+            id=word.id,
+            main_language=word.main_language,
+            second_language=word.second_language,
+            transcription=word.transcription
         )
 
-        user: User = await self.word_repo.update_user_by_id(user_id=user_id, user_update=user_update_hash_password)
-
-        return UserResponse(
-            id=user.id,
-            username=user.username,
-            email=user.email,
-            birthday=user.birthday
-        )
-
-    async def delete_user(self, user_id: int) -> str:
-        check_user_exist: bool = await self.check_user_exist(user_id=user_id)
+    async def delete_word(self, word_id: int) -> int:
+        check_user_exist: bool = await self.check_word_exist(word_id=word_id)
 
         if not check_user_exist:
-            raise Exception(f"Пользователь с id = `{user_id}` не был найден!")
+            raise Exception(f"Слово с id = `{word_id}` не было найдено!")
 
-        result: str = await self.word_repo.delete_user_by_id(user_id=user_id)
+        result: int = await self.word_repo.delete_word(word_id=word_id)
 
         return result
 
-    async def hash_password(self, password: str) -> str:
-        hashed_password = self.hash_generator.generate_hash_from_password(password=password)
-        return hashed_password
+    async def check_word_exist(self, word_id: int) -> bool:
+        word = await self.word_repo.get_word_by_id(word_id=word_id)
 
-    async def verify_password(self, password: str, hash_password: str) -> bool:
-        result = self.hash_generator.verify_password(password=password, hashed_password=hash_password)
-        return result
-
-    async def check_duplicate(self, user_email: str) -> bool:
-        user: User = await self.word_repo.get_user_by_email(user_email=user_email)
-
-        if user:
-            return True
-        else:
-            return False
-
-    async def check_user_exist(self, user_id: int) -> bool:
-        user: User = await self.word_repo.get_user_by_id(user_id=user_id)
-
-        if user:
+        if word:
             return True
         else:
             return False
