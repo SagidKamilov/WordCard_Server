0b8d1f3cb810df5b60387117b7408d2b3e3c1dd0
SagidKamilov <89615854074a@gmail.com>
2024-06-09 23:58:28 +0300

    Снял ключ уникальности для имени категории

--- src/model/category.py
+++ src/model/category.py
@@ -7,6 +7,6 @@ from src.model.base_model import Base
 class Category(Base):
     __tablename__ = "category_word_card"
 
-    category_name: Mapped[str] = mapped_column(name="category_name", type_=String, nullable=False, unique=True)
+    category_name: Mapped[str] = mapped_column(name="category_name", type_=String, nullable=False, unique=False)
 
-    owner_id: Mapped[int] = mapped_column("owner_id", ForeignKey("user_word_card.id"))
+    owner_id: Mapped[int] = mapped_column("owner_id", ForeignKey("user_word_card.id", ondelete="CASCADE"))
