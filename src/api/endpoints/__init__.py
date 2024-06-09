8d14a4460cf6ff2edf16efcac2efc2e1689f83ee
SagidKamilov <89615854074a@gmail.com>
2024-06-09 10:31:42 +0300

    Подключил маршруты авторизации и регистрации

--- src/api/endpoints/__init__.py
+++ src/api/endpoints/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from src.api.endpoints.auth import router as auth_router
 from src.api.endpoints.user import router as user_router
 from src.api.endpoints.category import router as category_router
 from src.api.endpoints.word import router as word_router
@@ -7,6 +8,7 @@ from src.api.endpoints.word import router as word_router
 
 main_router = APIRouter()
 
+main_router.include_router(auth_router)
 main_router.include_router(user_router)
 main_router.include_router(category_router)
 main_router.include_router(word_router)
