0e32fd69f7b735246c34f843f42866c2039b2310
SagidKamilov <89615854074a@gmail.com>
2024-06-09 10:28:32 +0300

    Добавил маршрут для получение всех пользователей

--- src/api/endpoints/category.py
+++ src/api/endpoints/category.py
@@ -6,10 +6,10 @@ from src.api.dependencies import category_container
 from src.dto.category import CategoryCreate, CategoryUpdate, CategoryResponse
 
 
-router = APIRouter(prefix="/category", tags=["Действия над пользователем"])
+router = APIRouter(prefix="", tags=["Действия над категориями"])
 
 
-@router.post(path="/{user_id}", status_code=status.HTTP_201_CREATED, response_model=CategoryResponse)
+@router.post(path="/category/{user_id}", status_code=status.HTTP_201_CREATED, response_model=CategoryResponse)
 async def create_category(user_id: int, category_create: CategoryCreate):
     try:
         category = await category_container().create_category(user_id=user_id, category_create=category_create)
@@ -19,7 +19,7 @@ async def create_category(user_id: int, category_create: CategoryCreate):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(error_detail))
 
 
-@router.get(path="/{category_id}", status_code=status.HTTP_200_OK, response_model=CategoryResponse)
+@router.get(path="/category/{category_id}", status_code=status.HTTP_200_OK, response_model=CategoryResponse)
 async def get_category(category_id: int):
     try:
         category = await category_container().get_category(category_id=category_id)
@@ -29,7 +29,17 @@ async def get_category(category_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(error_detail))
 
 
-@router.post(path="/{category_id}/user/{user_id}", status_code=status.HTTP_200_OK, response_model=int)
+@router.get(path="/{user_id}/category", status_code=status.HTTP_200_OK, response_model=List[CategoryResponse])
+async def get_categories(user_id: int):
+    try:
+        categories = await category_container().get_categories(user_id=user_id)
+
+        return categories
+    except Exception as error_detail:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(error_detail))
+
+
+@router.post(path="/category/{category_id}/user/{user_id}", status_code=status.HTTP_200_OK, response_model=int)
 async def category_add_user(category_id: int, user_id: int):
     try:
         result = await category_container().add_user_in_category(user_id=user_id, category_id=category_id)
@@ -39,7 +49,7 @@ async def category_add_user(category_id: int, user_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(error_detail))
 
 
-@router.delete(path="/{category_id}/user/{user_id}", status_code=status.HTTP_200_OK, response_model=int)
+@router.delete(path="/category/{category_id}/user/{user_id}", status_code=status.HTTP_200_OK, response_model=int)
 async def category_add_user(category_id: int, user_id: int):
     try:
         result = await category_container().remove_user_from_category(user_id=user_id, category_id=category_id)
@@ -49,7 +59,7 @@ async def category_add_user(category_id: int, user_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(error_detail))
 
 
-@router.put(path="/{category_id}", status_code=status.HTTP_200_OK, response_model=CategoryResponse)
+@router.put(path="/category/{category_id}", status_code=status.HTTP_200_OK, response_model=CategoryResponse)
 async def update_category(category_id: int, category_update: CategoryUpdate):
     try:
         category = await category_container().update_category(category_id=category_id, category_update=category_update)
@@ -59,7 +69,7 @@ async def update_category(category_id: int, category_update: CategoryUpdate):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(error_detail))
 
 
-@router.delete(path="/{category_id}", status_code=status.HTTP_200_OK, response_model=int)
+@router.delete(path="/category/{category_id}", status_code=status.HTTP_200_OK, response_model=int)
 async def delete_category(category_id: int):
     try:
         result: int = await category_container().delete_category(category_id=category_id)
