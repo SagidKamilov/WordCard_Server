8985b287d9a758018c17fd404c8a19744a021b73
SagidKamilov <89615854074a@gmail.com>
2024-06-09 10:29:07 +0300

    Удалил связь между таблицами пользователя и категории

--- src/model/category.py
+++ src/model/category.py
@@ -10,4 +10,3 @@ class Category(Base):
     category_name: Mapped[str] = mapped_column(name="category_name", type_=String, nullable=False, unique=True)
 
     owner_id: Mapped[int] = mapped_column("owner_id", ForeignKey("user_word_card.id"))
-    owner: Mapped["user_word_card"] = relationship("user_word_card", back_populates="categories")
