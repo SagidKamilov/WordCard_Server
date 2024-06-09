f4df4d12df5eeb73361702d1f1f5e2fb4ba2bd16
SagidKamilov <89615854074a@gmail.com>
2024-06-09 10:36:28 +0300

    Удалил связь между таблицами категори и списком категории и пользователей

--- src/model/word.py
+++ src/model/word.py
@@ -12,4 +12,3 @@ class Word(Base):
     transcription: Mapped[str] = mapped_column(name="transcription", type_=String(255), nullable=True, unique=False)
 
     category_id: Mapped[int] = mapped_column("category_id", ForeignKey("category_word_card.id"))
-    category: Mapped["category_word_card"] = relationship("category_word_card", back_populates="words")
