5f01e10cfd854d1efab9441a08166823ee52d2a3
SagidKamilov <89615854074a@gmail.com>
2024-08-27 16:09:14 +0300

    Добавил типизацию для базового репозитория

--- src/repository/base_repo.py
+++ src/repository/base_repo.py
@@ -1,3 +1,8 @@
+from typing import Type
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
 class BaseRepository:
-    def __init__(self, session_maker):
-        self.db_session = session_maker()
+    def __init__(self, async_session: Type[AsyncSession]):
+        self.db_session = async_session
